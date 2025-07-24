8ddc825840f24c6b41a818d2f53ceecacd6f62ae
anantdeepsingh <156583531+anantdeepsingh@users.noreply.github.com>
2025-07-24 22:00:19 +0530

    Update Props-UseState.jsx

--- Props-UseState.jsx
+++ Props-UseState.jsx
@@ -1,1 +1,17 @@
+function Parent(){
+  const [count,setCount]= useState(0);
+  return (
+    <div>
+      <Child count={count} increment={()=-> setCount(count+1)}/>
+    </div>
+  );
+}
 
+function Child ({count,increment}){
+  return (
+    <div>
+      <p>count</p>
+       <button onClick={increment}>+</button>
+    </div>
+  );
+}
